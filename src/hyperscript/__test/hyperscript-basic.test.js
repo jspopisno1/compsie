a8780447976ef93949106fcaedb023cd4a8d6e72
Liang Zheng <jspop_no1@hotmail.com>
2017-09-24 23:17:06 +0800

    - test:mocha

---
+++ src/hyperscript/__test/hyperscript-basic.test.js
@@ -0,0 +1,21 @@
+var lazy = require('lazy-assert');
+
+/* global before, beforeEach, after, afterEach */
+describe('', function () {
+    before(function () {
+        lazy.setLocation(__filename);
+    });
+
+    beforeEach(function () {
+    });
+
+    after(function () {
+    });
+
+    afterEach(function () {
+    });
+
+    it('hyperscript basic', function () {
+
+    });
+});
